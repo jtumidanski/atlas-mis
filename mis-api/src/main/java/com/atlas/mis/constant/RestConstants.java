fce55983830f7d33113b71cd6f7db89a5d639561
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-07 11:36:13 -0500

    Add constant for accessing rest service.

---
+++ mis-api/src/main/java/com/atlas/mis/constant/RestConstants.java
@@ -0,0 +1,8 @@
+package com.atlas.mis.constant;
+
+public final class RestConstants {
+   private RestConstants() {
+   }
+
+   public static final String SERVICE = "mis";
+}
--- mis-base/src/main/java/com/atlas/mis/Server.java
+++ mis-base/src/main/java/com/atlas/mis/Server.java
@@ -2,13 +2,13 @@ package com.atlas.mis;
 
 import java.net.URI;
 
+import com.atlas.mis.constant.RestConstants;
 import com.atlas.shared.rest.RestServerFactory;
-import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
 
 public class Server {
    public static void main(String[] args) {
-      URI uri = UriBuilder.host(RestService.MAP_INFORMATION).uri();
+      URI uri = UriBuilder.host(RestConstants.SERVICE).uri();
       RestServerFactory.create(uri, "com.atlas.mis.rest");
    }
 }
